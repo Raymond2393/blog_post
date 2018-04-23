cd8bf45a3910ef34276cee1b023a69285cd9c6c6
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-23 08:52:51 +0300

    add def password

--- app/models.py
+++ app/models.py
@@ -23,19 +23,19 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You cannot read the password attribute')
-#
-#     @password.setter
-#     def password(self, password):
-#         self.pass_secure = generate_password_hash(password)
-#
-#     def verify_password(self, password):
-#         return check_password_hash(self.pass_secure, password)
-#
-#     def __repr__(self):
-#         return f'User {self.username}'
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
 #
 #
 # class Role(db.Model):
