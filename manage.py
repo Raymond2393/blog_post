5ee6af2797310e6570d3e4766eb436c1b8d96e91
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-23 09:11:30 +0300

    add a unit test

--- manage.py
+++ manage.py
@@ -6,19 +6,19 @@ from flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
 
-# manager = Manager(app)
-# manager.add_command('server', Server)
-#
-# migrate = Migrate(app, db)
-# manager.add_command('db', MigrateCommand)
-#
-#
-# @manager.command
-# def test():
-#     """Run unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
+manager = Manager(app)
+manager.add_command('server', Server)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Run unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 #
 #
 # @manager.shell
