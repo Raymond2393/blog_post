e456b329d334fbce29896cf3e7cfffbad64e5cef
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-23 08:59:15 +0300

    add def mail message

--- app/email.py
+++ app/email.py
@@ -2,10 +2,10 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-# def mail_message(subject,template,to,**ray):
-#     sender_email = 'rokwenda090@gmail.com'
-#
-#     email = Message(subject, sender=sender_email, recipients=[to])
-#     email.body= render_template(template + ".txt",**ray)
-#     email.html = render_template(template + ".html",**ray)
-#     mail.send(email)
+def mail_message(subject,template,to,**ray):
+    sender_email = 'rokwenda090@gmail.com'
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**ray)
+    email.html = render_template(template + ".html",**ray)
+    mail.send(email)
